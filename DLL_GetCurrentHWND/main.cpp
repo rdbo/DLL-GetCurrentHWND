84f4d4e7ab2cdcd7c6d26b6074f59fdec1c82ded
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-01-09 14:41:26 -0300

    Add files via upload

---
+++ DLL_GetCurrentHWND/main.cpp
@@ -0,0 +1,43 @@
+#include <Windows.h>
+
+static HWND window;
+
+BOOL CALLBACK EnumWindowsCallback(HWND handle, LPARAM lParam)
+{
+	DWORD wndProcId;
+	GetWindowThreadProcessId(handle, &wndProcId);
+
+	if (GetCurrentProcessId() != wndProcId)
+		return TRUE; // skip to next window
+
+	window = handle;
+	return FALSE; // window found abort search
+}
+
+HWND GetProcessWindow()
+{
+	window = NULL;
+	EnumWindows(EnumWindowsCallback, NULL);
+	return window;
+}
+
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	HWND wnd = GetProcessWindow();
+	MessageBox(wnd, "It Works!", "MainThread", MB_OK);
+	return TRUE;
+}
+
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
